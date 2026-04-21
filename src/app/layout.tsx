5fc31cca176c7837456a41eed10c44fb3956b92a
michaelpreiner <51121829+michaelpreiner@users.noreply.github.com>
2026-04-21 18:40:22 +0200

    fix: hardcode apple-touch-icon link in head to ensure iOS picks it up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
     <html lang="de">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body>{children}</body>
     </html>
